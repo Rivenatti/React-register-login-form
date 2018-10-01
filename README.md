0057370df25be6bac9c8a4849156586884664eba
Marcin <Rivenatti@gmail.com>
2018-10-01 11:11:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Get started
+* Install all React project dependencies
+> npm install
+
+* Start the development server with
+> npm start
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">Get starteds</p>
+      </div>
+    );
+  }
+}
+
+export default App;
